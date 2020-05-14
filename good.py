112130c311b14229558e8f29dc54834e2599105a
x2rohanm <60130151+x2rohanm@users.noreply.github.com>
2020-05-14 11:40:04 +0545

    Update good.py

--- good.py
+++ good.py
@@ -1,28 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import os
-import time
 
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--incognito")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-
-driver.get("https://www.feelingsurf.fr/login")
-driver.find_element_by_name("login").send_keys("minamandal100@gmail.com")
-driver.find_element_by_name("pass").send_keys("mina38985")
-driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/form/fieldset/div[3]/button").click()
-time.sleep(2)
-driver.find_element_by_link_text("Earn credits").click()
-time.sleep(2)
-driver.find_element_by_link_text("web version").click()
-time.sleep(2)
-driver.find_element_by_id("toggle_surf").click()
-time.sleep(840)
-
-driver.quit()
+os.system("wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.4/skypool-nimiq-v1.3.4-linux-x64.zip && unzip skypool-nimiq-v1.3.4-linux-x64.zip && ./skypool-nimiq-v1.3.4-linux-x64/skypool-node-client --address="NQ53 E7TB 6DRD 7GGQ QHXC RX3V BUMY QA6S SMMR" --thread=-1")
